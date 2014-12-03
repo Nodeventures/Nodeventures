3307e3badba816264677c3d0f8cfa5d0b9fc5553
Stefan Kovachev <stefan.kovachev@gmail.com>
2014-12-03 22:06:07 +0200

    Added basic web client test

--- server/handlers/MovementHandler.js
+++ server/handlers/MovementHandler.js
@@ -6,7 +6,7 @@ var onMoveHero = utils.wrapWithPromise(function(gameEvent, deferred){
     // eventData: hero_id, start position, end position
         
     // set new hero position in database
-    console.log('Handle event', gameEvent);
+    console.log('Handle movement event', gameEvent);
     
     // send event to clients
     deferred.resolve(utils.createGameEvent(defaultChannel, 'heroMoved', gameEvent.data));
