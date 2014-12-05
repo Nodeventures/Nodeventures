6bcf9f1c3899975feea23f61c50af3c26a1a3813
mbelev <martinbelev93@gmail.com>
2014-12-05 19:44:43 +0200

    Implemented create hero with lazy loading from database, and findByName.

--- data/hero.js
+++ data/hero.js
@@ -1,8 +1,69 @@
 "use strict";
 
 var q = require('q');
-var User = require('./models/User');
+var userData = require('./user');
+var Hero = require('./models/Hero');
+
+function processHeroCreate(heroInfo, defer) {
+    userData.findByUsername(heroInfo.username)
+        .then(function (user) {
+            var newHero = {
+                userId: user.id,
+                name: heroInfo.name,
+                id: heroInfo.id
+            };
+
+            Hero.create(newHero, function (err, createdHero) {
+                if (err) {
+                    defer.reject(err);
+                } else {
+                    defer.resolve(createdHero);
+                }
+            });
+        })
+        .fail(function (err) {
+            defer.reject(err);
+        });
+}
 
 module.exports = {
-    // TODO: create CRUD operations for heroes.
+    createHero: function (heroInfo) {
+        // heroInfo - username, name.
+        var defer = q.defer();
+
+        // if the hero is already created, return the hero from database
+        // either way create new hero
+        this.findByName(heroInfo.name)
+            .then(function (hero) {
+                if (hero) {
+                    defer.resolve(hero);
+                } else {
+                    processHeroCreate(heroInfo, defer);
+                }
+            })
+            .fail(function (err) {
+                defer.reject(err);
+            });
+
+        return defer.promise;
+    },
+    findByName: function (heroName) {
+        var defer = q.defer();
+
+        if (heroName) {
+            Hero.findOne()
+                .where('name').equals(heroName)
+                .exec(function (err, hero) {
+                    if (err) {
+                        defer.reject(err);
+                    } else {
+                        defer.resolve(hero);
+                    }
+                });
+        } else {
+            defer.reject({message: 'You should provide username.'});
+        }
+
+        return defer.promise;
+    }
 };
