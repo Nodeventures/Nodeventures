4084cc75997bf4dfdb4964173d82d8c065466de0
mbelev <martinbelev93@gmail.com>
2014-12-05 19:47:02 +0200

    LoginHandler now uses the hero data, so that after login/register can create new hero is the user has not got one, or get the hero for the current user.

--- server/handlers/LoginHandler.js
+++ server/handlers/LoginHandler.js
@@ -1,75 +1,60 @@
 var utils = require('./utils'),
     defaultChannel = '/system';
 
-var userData = require('../../data').user;
-
-var heroIdCounter = 1;
+var data = require('../../data');
 
 var onUserLogin = utils.wrapWithPromise(function (gameEvent, deferred) {
     // eventData: username, password
 
     console.log('User logged in or is registering', gameEvent);
 
-    userData.registerUser(gameEvent.data)
+    data.user.registerUser(gameEvent.data)
         .then(function (registeredUser) {
-            // fill out what you can from the database
-            var data = {
+            data.hero.createHero({
+                username: registeredUser.username,
+                name: registeredUser.username
+            }).then(function (createdHero) {
+                console.log(createdHero);
+                // fill out what you can from the database
+                var eventData = {
 
-                // load user data from db
-                user: {
-                    username: registeredUser.username
-                },
-
-                // all images that need to be loaded before the scene is initialized
-                images: {
-                    heroSprite: "assets/tileset/space_guy.png",
-                    tileSet: "assets/tileset/free_tileset_CC.png",
-                },
-
-                // hero information
-                hero: {
-                    name: registeredUser.username,
-                    heroSprite: "assets/tileset/space_guy.png",
-                    position: {
-                        x: 320,
-                        y: 320,
-                        map: 'fields'
-                    },
-                    animations: {
-                        idle: [
-                            [1, 2],
-                        ],
-
-                        walk: [
-                            [0, 2],
-                            [1, 2],
-                            [2, 2]
-                        ],
+                    // load user data from db
+                    user: {
+                        username: registeredUser.username
                     },
 
-                    id: heroIdCounter++
-                },
+                    // all images that need to be loaded before the scene is initialized
+                    images: {
+                        heroSprite: "assets/tileset/space_guy.png",
+                        tileSet: "assets/tileset/free_tileset_CC.png",
+                    },
 
-                // map object loaded based on hero's position.map field
-                map: {
-                    key: 'fields',
+                    // hero information
+                    hero: createdHero,
 
-                    tileSize: 32,
-                    tileSet: "assets/tileset/free_tileset_CC.png",
-                    width: 640,
-                    height: 640,
+                    // map object loaded based on hero's position.map field
+                    map: {
+                        key: 'fields',
 
-                    tilesConfig: {
-                        imageX: 5,
-                        imageY: 18,
-                        stepsHorizontalAllowed: 2,
-                        stepsVerticalAllowed: 2,
+                        tileSize: 32,
+                        tileSet: "assets/tileset/free_tileset_CC.png",
+                        width: 640,
+                        height: 640,
+
+                        tilesConfig: {
+                            imageX: 5,
+                            imageY: 18,
+                            stepsHorizontalAllowed: 2,
+                            stepsVerticalAllowed: 2,
+                        }
                     }
-                }
-            };
+                };
 
-            // send event to clients
-            deferred.resolve(utils.createGameEvent(defaultChannel, 'userLoggedIn', data));
+                // send event to clients
+                deferred.resolve(utils.createGameEvent(defaultChannel, 'userLoggedIn', eventData));
+            }).fail(function (err) {
+                deferred.reject(err);
+            });
         })
         .fail(function (err) {
             deferred.reject(err);
