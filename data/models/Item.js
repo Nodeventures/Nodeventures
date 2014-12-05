67ff9ca187e235b675a20d54176f8f1ddbeea4ee
mbelev <martinbelev93@gmail.com>
2014-12-05 16:40:42 +0200

    Item database model added.

---
+++ data/models/Item.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var mongoose = require('mongoose');
+
+var itemSchema = new mongoose.Schema({
+    // item has type, for example weapon, and the key will be the item name (example: sword, axe, etc.)
+    type: {type: String, required: true},
+    key: {type: String, required: true, unique: true},
+    description: String,
+    // the modifiers for hero stats change after item is picked up
+    modifiers: {
+        health: {type: Number, required: true},
+        attack: {type: Number, required: true},
+        defense: {type: Number, required: true}
+    }
+});
+
+var Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
