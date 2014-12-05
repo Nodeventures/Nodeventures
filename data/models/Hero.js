3fde2fcb3c8fabef8abcbf285b53814f13211479
mbelev <martinbelev93@gmail.com>
2014-12-05 16:40:23 +0200

    Added map to position. More fields added - heroSprite, animations (which have idle and walk).

--- data/models/Hero.js
+++ data/models/Hero.js
@@ -4,17 +4,22 @@ var mongoose = require('mongoose');
 
 // If we have more different kind of heroes can add type.
 // Different heroes will start with different health, attack and defense points.
-// Maybe should hold information about images
 var heroSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
     position: {
         x: Number,
-        y: Number
+        y: Number,
+        map: {type: String, default: 'fields'}
     },
-    health: {type: Number, required: true},
-    attack: {type: Number, required: true},
-    defense: {type: Number, required: true},
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    health: {type: Number, required: true, default: 100},
+    attack: {type: Number, required: true, default: 0},
+    defense: {type: Number, required: true, default: 0},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    heroSprite: {type: String, default: 'assets/tileset/space_guy.png'},
+    animations: {
+        idle: {type: [[Number]], default: [[1, 2]]},
+        walk: {type: [[Number]], default: [[0, 2], [1, 2], [2, 2]]}
+    }
 });
 
 var Hero = mongoose.model('Hero', heroSchema);
