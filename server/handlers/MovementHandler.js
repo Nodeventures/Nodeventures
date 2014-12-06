ecd82a4290039f140c740117f4bdddddec02ed24
Stefan Kovachev <stefan.kovachev@gmail.com>
2014-12-06 18:10:13 +0200

    Added methods to Movement handler

--- server/handlers/MovementHandler.js
+++ server/handlers/MovementHandler.js
@@ -4,13 +4,27 @@ var utils = require('./utils'),
 var onMoveHero = utils.wrapWithPromise(function(gameEvent, deferred){
     // eventData: hero_id, start position, end position
         
-    // set new hero position in database
+    // TODO set new hero position in database
     console.log('Handle movement event', gameEvent);
     
     // send event to clients
     deferred.resolve(utils.createGameEvent(defaultChannel, 'heroMoved', gameEvent.data));
 });
 
+var onMapChanged = utils.wrapWithPromise(function(gameEvent, deferred){
+    // eventData: hero_id, map_key
+        
+    // TODO change map_key in hero.position
+    // set hero.position x and y to map startingPoint x, y
+    console.log('Handle map change event', gameEvent);
+
+    // TODO: load new map data for hero and send back
+    
+    // send event to clients
+    deferred.resolve(utils.createGameEvent(defaultChannel, 'heroChangedMap', gameEvent.data));
+});
+
 module.exports = {
-    onMoveHero: onMoveHero
+    onMoveHero: onMoveHero,
+    onMapChanged: onMapChanged
 };
