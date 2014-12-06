aa1aea01aea4fe1b19a5b3455c959a133d10ecd6
Stefan Kovachev <stefan.kovachev@gmail.com>
2014-12-07 00:31:05 +0200

    Refactored login logic to load map data correctly

--- server/handlers/LoginHandler.js
+++ server/handlers/LoginHandler.js
@@ -8,6 +8,49 @@ var onUserLogin = utils.wrapWithPromise(function (gameEvent, deferred) {
 
     console.log('User logged in or is registering', gameEvent);
 
+    var eventData = {};
+    data.user.registerUser(gameEvent.data)
+
+        // get user object
+        .then(function(registeredUser){
+            eventData.user = {
+                username: registeredUser.username
+            };
+
+            return data.hero.createHero({
+                username: registeredUser.username,
+                name: registeredUser.username
+            });
+        })
+
+        // get hero
+        .then(function(createdHero){
+            eventData.hero = createdHero;
+            return data.map.loadMapByKey(createdHero.position.map);
+        })
+
+        // load map based on hero position
+        .then(function(map){
+            eventData.map = map;
+        })
+
+        // forward any errors back
+        .catch(function(error){
+            deferred.reject(error);
+        })
+
+        // return event
+        .done(function(){
+
+            // mark images / assets that need loading
+            eventData.images = [
+                eventData.hero.heroSprite,
+                eventData.map.tileSet
+            ];
+
+            deferred.resolve(utils.createGameEvent(defaultChannel, 'userLoggedIn', eventData));
+        });
+
     data.user.registerUser(gameEvent.data)
         .then(function (registeredUser) {
             data.hero.createHero({
@@ -61,14 +104,25 @@ var onUserLogin = utils.wrapWithPromise(function (gameEvent, deferred) {
         });
 });
 
-var onUserLogout = utils.wrapWithPromise(function (gameEvent, deferred) {
-    // eventData: username, hero_id
-
-    // TODO
+function sendLogoutEvent(gameEvent, deferred) {
     console.log('User logged out', gameEvent);
-
     // notify other clients that user has logged out
     deferred.resolve(utils.createGameEvent(defaultChannel, 'userLoggedOut', gameEvent.data));
+}
+
+var onUserLogout = utils.wrapWithPromise(function (gameEvent, deferred) {
+
+    // eventData: username, hero_id
+    data.user.setHeroStatus('offline', gameEvent.data)
+        .done(function(){
+            // notify other clients that user has logged out
+            sendLogoutEvent(gameEvent, deferred);
+        })
+        .fail(function(err){
+            // logout user anyway
+            sendLogoutEvent(gameEvent, deferred);
+        });
+    
 });
 
 module.exports = {
