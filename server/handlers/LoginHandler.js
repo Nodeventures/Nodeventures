eda95146c7e60c7f1afd4a1eedfee4f78b419833
Stefan Kovachev <stefan.kovachev@gmail.com>
2014-12-07 00:49:13 +0200

    Cleaned up login handler

--- server/handlers/LoginHandler.js
+++ server/handlers/LoginHandler.js
@@ -50,58 +50,6 @@ var onUserLogin = utils.wrapWithPromise(function (gameEvent, deferred) {
 
             deferred.resolve(utils.createGameEvent(defaultChannel, 'userLoggedIn', eventData));
         });
-
-    data.user.registerUser(gameEvent.data)
-        .then(function (registeredUser) {
-            data.hero.createHero({
-                username: registeredUser.username,
-                name: registeredUser.username
-            }).then(function (createdHero) {
-                console.log(createdHero);
-                // fill out what you can from the database
-                var eventData = {
-
-                    // load user data from db
-                    user: {
-                        username: registeredUser.username
-                    },
-
-                    // all images that need to be loaded before the scene is initialized
-                    images: {
-                        heroSprite: "assets/tileset/space_guy.png",
-                        tileSet: "assets/tileset/free_tileset_CC.png",
-                    },
-
-                    // hero information
-                    hero: createdHero,
-
-                    // map object loaded based on hero's position.map field
-                    map: {
-                        key: 'fields',
-
-                        tileSize: 32,
-                        tileSet: "assets/tileset/free_tileset_CC.png",
-                        width: 640,
-                        height: 640,
-
-                        tilesConfig: {
-                            imageX: 5,
-                            imageY: 18,
-                            stepsHorizontalAllowed: 2,
-                            stepsVerticalAllowed: 2,
-                        }
-                    }
-                };
-
-                // send event to clients
-                deferred.resolve(utils.createGameEvent(defaultChannel, 'userLoggedIn', eventData));
-            }).fail(function (err) {
-                deferred.reject(err);
-            });
-        })
-        .fail(function (err) {
-            deferred.reject(err);
-        });
 });
 
 function sendLogoutEvent(gameEvent, deferred) {
