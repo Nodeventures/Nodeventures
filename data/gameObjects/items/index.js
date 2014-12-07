42bea88d656b3f4aee8df12e82629a41a53730e8
mbelev <martinbelev93@gmail.com>
2014-12-07 15:37:26 +0200

    All items as array.

---
+++ data/gameObjects/items/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var greatSwordOfAwesomeness = require('./great-sword-of-awesomeness.json');
+var plainLeatherArmor = require('./plain-leather-armor.json');
+var sturdyBuckleOfThorns = require('./sturdy-buckle-of-thorns.json');
+
+module.exports = [
+    greatSwordOfAwesomeness,
+    plainLeatherArmor,
+    sturdyBuckleOfThorns
+];
