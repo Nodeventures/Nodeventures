d399c73abd41e259a5e4e22fb5cbd15d6e46abe5
Stefan Kovachev <stefan.kovachev@gmail.com>
2014-12-09 11:30:51 +0200

    [web] Added map image object

---
+++ clients/web/assets/ui/MapImage.js
@@ -0,0 +1,34 @@
+(function() {
+
+    Nv.MapImage = function(map, config) {
+        Nv.MapObject.call(this, map, config.width, config.height, config.position, config);
+
+        this.name = config.name;
+        this.key = config.key;
+
+        var image = new Kinetic.Image({
+            x: 0,
+            y: 0,
+            image: Nv.sessionInstance().images[config.key],
+            width: config.width,
+            height: config.height
+        });
+
+        this.image = image;
+        this.config = config;
+
+        this.onClickHandler = config.onClickHandler || function(){};
+
+        this.add(this.image);
+    };
+
+    Nv.MapImage.prototype = {
+        mapObjectClicked: function() {
+            return this.onClickHandler();
+        },
+
+    };
+
+    Kinetic.Util.extend(Nv.MapImage, Nv.MapObject);
+
+})();
